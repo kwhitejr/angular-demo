d93e91fda62a58977c43bf9fa19f9db0a25bd834
kwhitejr <kwhitejr.edu@gmail.com>
2016-02-24 20:12:23 -1000

    angular set up

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-  res.render('index');
-});
+// app.get('/', function (req, res) {
+//   res.render('index');
+// });
 
 var server = app.listen(8000, function() {
   console.log('Listening to port', server.address().port);
