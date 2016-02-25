558ab9970d999515ffb7f0ee0308d0f0312c557b
kwhitejr <kwhitejr.edu@gmail.com>
2016-02-24 21:47:54 -1000

    created character version factory

--- public/js/controller.js
+++ public/js/controller.js
@@ -3,9 +3,11 @@ var myApp = angular.module('myApp');
 myApp.controller('MyController', [
   '$scope',
   'mainCharacter',
-  function ($scope, mainCharacter) {
+  'characterVersionFactory',
+  function ($scope, mainCharacter, characterVersionFactory) {
     $scope.myFirstName = 'Ser Kevin of Haus Whyte';
     $scope.myModel = "Ready Player One";
     $scope.mainCharacter = mainCharacter;
+    $scope.characterVersion = characterVersionFactory;
   }
 ]);
